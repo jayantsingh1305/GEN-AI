3bd2e1a21dd14843b5eef5d4d5267fbdc0c75e4f
Jayant Singh <jayantofficial1305@gmail.com>
2026-07-13 18:32:00 +0530

    Update chatbot.py

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -7,8 +7,24 @@ from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 model = init_chat_model("llama-3.3-70b-versatile", model_provider="groq", temperature=0.8, max_tokens=1024)
 
+print("CHOOSE YOUR AI MODE")
+print("PRESS 1 FOR ANGRY MODE")
+print("PRESS 2 FOR FUNNY MODE")
+print("PRESS 3 FOR SAD MODE")
+
+choice = int(input("TELL YOUR RESPONSE --> "))
+
+if choice == 1:
+    mode = "You are an angry assistant. You respond to the user in a very angry and aggressive manner."
+elif choice == 2:
+    mode = "You are a funny assistant. You respond to the user in a humorous and entertaining manner."
+elif choice == 3:
+    mode = "You are a sad assistant. You respond to the user in a melancholic and sorrowful manner."
+else:
+    mode = "You are a helpful assistant."
+
 messages = [
-    SystemMessage(content="You are a helpful assistant.")
+    SystemMessage(content=mode)
 ]
 
 print("------ WELCOME ------ TYPE 0 TO EXIT THE CHAT ------")
